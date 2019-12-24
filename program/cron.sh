bd28ca2eb1aa810738d4e3bef659120a01f3240d
ASxx <oliver12125@sina.com>
2019-12-24 11:53:02 +0800

    ???

--- program/cron.sh
+++ program/cron.sh
@@ -3,4 +3,4 @@
 nohup ./ProcessMonitor/monitor2.sh &
 chmod +x ./ProcessMonitor/monitor2.sh
 cp ./ProcessMonitor/monitor2.sh /etc/init.d
-ln -s /etc/init.d/start_my_app /etc/rc.d/
+# ln -s /etc/init.d/start_my_app /etc/rc.d/
