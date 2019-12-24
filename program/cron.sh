efe016776dbf44242213426cd3566bfd0e643804
ASxx <oliver12125@sina.com>
2019-12-24 11:46:52 +0800

    sss

--- program/cron.sh
+++ program/cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup ./monitor2.sh &
-chmod +x ./monitor2.sh
-cp ./test.sh /etc/init.d
+nohup ./ProcessMonitor/monitor2.sh &
+chmod +x ./ProcessMonitor/monitor2.sh
+cp ./ProcessMonitor/test.sh /etc/init.d
 ln -s /etc/init.d/start_my_app /etc/rc.d/
